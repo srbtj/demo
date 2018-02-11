d42d2da9b5ad6983cacbd817b4f952a6bce123ce
tangjiang <tangjiang@analysys.com.cn>
2018-02-11 13:45:58 +0800

    commit

---
+++ src/main/java/com/srbtj/demo/config/DruidConfig.java
@@ -0,0 +1,30 @@
+package com.srbtj.demo.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+
+        // 登录查看信息的账号密码
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
--- src/main/java/com/srbtj/demo/config/ShiroConfig.java
+++ src/main/java/com/srbtj/demo/config/ShiroConfig.java
@@ -1,12 +1,29 @@
 package com.srbtj.demo.config;
 
 import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+import com.srbtj.demo.entity.Resources;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @Configuration
 public class ShiroConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.password}")
+    private String password;
 
     @Bean
     public static LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
@@ -19,5 +36,48 @@ public class ShiroConfig {
     @Bean
     public ShiroDialect shiroDialect() {
         return new ShiroDialect();
+    }
+
+    /**
+     * ShiroFilterFactoryBean 处理拦截资源文件问题。
+     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，因为在
+     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
+     *
+     Filter Chain定义说明
+     1、一个URL可以配置多个Filter，使用逗号分隔
+     2、当设置多个过滤器时，全部验证通过，才视为通过
+     3、部分过滤器可指定参数，如perms，roles
+     *
+     */
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+
+        System.out.print("ShrioConfiguration.shiroFilter()");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+
+        // 设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        // 登录成功后要跳转的链接
+        shiroFilterFactoryBean.setSuccessUrl("/usersPage");
+        // //未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        // 拦截器
+        Map<String, String> filterChainMap = new LinkedHashMap<String, String>();
+        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainMap.put("/logout", "logout");
+        filterChainMap.put("/css/**","anon");
+        filterChainMap.put("/js/**","anon");
+        filterChainMap.put("/img/**","anon");
+        filterChainMap.put("/font-awesome/**","anon");
+        //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
+        //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        //自定义加载权限资源关系
+        // todo
+
+        filterChainMap.put("/**", "authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainMap);
+        return shiroFilterFactoryBean;
     }
 }
